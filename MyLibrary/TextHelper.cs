ee1c58a0b8d1f5666473e55d1e679044a586217a
BoiSa <boikoff.aleksandr2016@yandex.ru>
2023-05-23 19:31:37 +0300

    Попправил solution и удалил лишний метод

--- MyLibrary/TextHelper.cs
+++ MyLibrary/TextHelper.cs
@@ -17,35 +17,5 @@ namespace MyLibrary
 
             return dictionaryWords ?? new Dictionary<string, int>();
         }
-
-        private static async Task<Dictionary<string, int>> GetCountUniqueWordsAsync(string pathFile)
-        {
-            List<string> words = new List<string>();
-            Regex regex = new Regex(@"\p{L}+");
-            StreamReader? reader = null;
-
-            try
-            {
-                reader = new(pathFile, Encoding.UTF8);
-                string? strLine;
-                while ((strLine = await reader.ReadLineAsync()) != null)
-                {
-                    words.AddRange(regex.Matches(strLine).Select(w => w.Value.ToLower()));
-                }
-            }
-            catch (Exception)
-            {
-                await Console.Out.WriteLineAsync("Ошибка чтение файла");
-            }
-            finally
-            {
-                reader?.Close();
-            }
-
-            var dictionaryWords = words.GroupBy(m => m).ToDictionary(w => w.Key, c => c.Count())
-                .OrderByDescending(m => m.Value).ToDictionary(m => m.Key, m => m.Value);
-
-            return dictionaryWords;
-        }
     }
 }
